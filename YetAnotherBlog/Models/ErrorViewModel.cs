ecc8bbf6e91593b9ecfb24b24a47bfe340d0a6ce
Chris Johnston <cjohnsto330@gmail.com>
2018-02-27 12:34:06 -0600

    Add project files.

---
+++ YetAnotherBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YetAnotherBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
