8ff650d7b8d0daa61298d9f87bd604adbaad5999
Chris Johnston <cjohnsto330@gmail.com>
2018-02-27 13:34:09 -0600

    Added PostModel.cs

---
+++ YetAnotherBlog/Models/PostModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetAnotherBlog.Models
+{
+    public class PostModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        public string Post { get; set; }
+        public string Tags { get; set; }
+        public bool AllowResponses { get; set; }
+        public int ResponseCount { get; set; }
+        public DateTime TimePosted { get; set; }
+
+        public bool Edited { get; set; }
+        public DateTime LastEdited { get; set; }
+    }
+}
